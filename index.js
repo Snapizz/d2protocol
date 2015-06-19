8bb2b79b5149ed0297fcf04b0650d88a240fbd46
Snapiz <snapizz97@gmail.com>
2015-06-19 14:25:47 -0700

    load enums

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-module.exports = {
+var glob = require('glob'),
+    path = require('path');
+
+var d2protocol = {
   messageReceiver: require('./lib/message-receiver.js'),
   protocolTypeManager: require('./lib/protocol-type-manager.js')
-}
+};
+
+module.exports = d2protocol;
+
+glob(path.join(__dirname, 'enums/**/*.js'), function( err, files ) {
+  if(err){
+    throw err;
+  }
+  files.forEach(function (file) {
+    var e = require(file);
+    d2protocol[path.basename(file, '.js')] = e;
+  });
+});
